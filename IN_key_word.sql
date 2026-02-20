8a211f5d798e4ff6e78a3412f20afa535ce68864
md-habibullah-99 <habibullahhisam@gmail.com>
2026-02-20 10:39:30 +0600

    do some clean up

--- IN_key_word.sql
+++ IN_key_word.sql
@@ -1,3 +1,4 @@
+-- the table element or table is not available on any of the data base here
 SELECT "title" FROM "books" WHERE "id" IN (
   SELECT "book_id" FROM "authored"
   WHERE "author_id" = (
--- intro.sql
+++ intro.sql
@@ -1,3 +1,5 @@
+-- To check tables in db run ".tables"
+.tables
 -- SELECT "title","author" FROM "books";
 -- SELECT COUNT(*) FROM "books";
-SELECT "title" FROM "books" ORDER BY "title" DESC;
+-- SELECT "title" FROM "books" ORDER BY "title" DESC;
